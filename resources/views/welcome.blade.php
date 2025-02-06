ad98f0cf89ea03efd7dc1eddfaf074cda9a9da0b
kojimaro <ryokojima7@gmail.com>
2025-02-06 17:03:35 +0900

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -168,6 +168,7 @@
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        TEST KOJIMA
                     </footer>
                 </div>
             </div>
